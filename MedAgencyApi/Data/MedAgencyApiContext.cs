d133c0350895de0ebddcdf2049450d1a96de0aa7
criostaze <criostaze@yandex.ru>
2020-09-24 20:10:22 +0400

    refactoring part1

--- MedAgencyApi/Data/MedAgencyApiContext.cs
+++ MedAgencyApi/Data/MedAgencyApiContext.cs
@@ -5,6 +5,7 @@ using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
 using MedAgencyApi.Models;
 using System.ComponentModel.DataAnnotations;
+using MedAgencyApi.Data.Interfaces;
 
 namespace MedAgencyApi.Data
 {
