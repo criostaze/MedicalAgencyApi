a7d114d09626b69f3f7efd3fc3256eeba130a3c1
criostaze <criostaze@yandex.ru>
2020-10-21 17:55:56 +0400

    auth IS4 update

--- MedAgencyClient/medical-agency-client/src/app/app-routing.module.ts
+++ MedAgencyClient/medical-agency-client/src/app/app-routing.module.ts
@@ -7,15 +7,36 @@ import { ErrorPageComponent } from './error-page/error-page.component';
 import { AboutPageComponent } from './about-page/about-page.component';
 import { WelcomePageComponent } from './welcome-page/welcome-page.component';
 import { NotFoundComponent } from './not-found/not-found.component';
+import { AuthGuardService } from 'src/services/auth-guard.service';
 
 const routes: Routes = [
+  {
+    path: '',
+    children: []
+  },
   { path: 'about', component: AboutPageComponent},
-  { path: 'card/:id', component: CardsViewComponent},
-  { path: 'account/:id', component: AccountPageComponent},
-  { path: 'error', component: ErrorPageComponent},
-  { path: 'welcome', component: WelcomePageComponent},
-  { path: '^^', component: NotFoundComponent},
-  {path: '', redirectTo: '/welcome', pathMatch: 'full'}
+  {
+    path: 'card/:id', component: CardsViewComponent,
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: 'account/:id', component: AccountPageComponent,
+    canActivate: [AuthGuardService]
+  },
+  {
+    path: 'error',
+    component: ErrorPageComponent},
+  {
+    path: 'welcome',
+    component: WelcomePageComponent
+  },
+  {
+    path: '^^',
+    component: NotFoundComponent
+  },
+  {
+    path: '', redirectTo: '/welcome',
+    pathMatch: 'full'}
 ];
 
 @NgModule({
